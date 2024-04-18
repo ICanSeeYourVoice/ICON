1ee2d3ac41c8ca00f3d4e6ff8ebd118559a96a2e
김현영 <hggew@naver.com>
2024-04-18 12:10:19 +0900

    Revert "feat: 예외추가, fcm 설정"
    This reverts commit 1d8c8bb2ebf48b64e7795e63373fa93b686521fa.

--- back/src/main/java/com/ssafy/icon/config/config.java
+++
@@ -1,4 +0,0 @@
-package com.ssafy.icon.config;
-
-public class config {
-}
--- back/src/main/java/com/ssafy/icon/domain/user/exception/UserErrorCode.java
+++ back/src/main/java/com/ssafy/icon/domain/user/exception/UserErrorCode.java
@@ -1,17 +1,4 @@
 package com.ssafy.icon.domain.user.exception;
 
-import org.springframework.http.HttpStatus;
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@AllArgsConstructor
-@Getter
-public enum UserErrorCode {
-
-	USER_NOT_FOUND("사용자를 찾을 수 없습니다.", HttpStatus.BAD_REQUEST);
-
-	private final String message;
-	private final HttpStatus httpStatus;
-
+public class UserErrorCode {
 }
