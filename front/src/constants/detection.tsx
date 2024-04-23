275a79c7aff5925094fc546e3ed597585f2b7a9c
nayeon <kknyapple@gmail.com>
2024-04-23 12:19:28 +0900

    style: ReactButton css 수정

--- front/src/constants/detection.tsx
+++ front/src/constants/detection.tsx
@@ -1,40 +1,48 @@
+import baby from "../assets/svgs/detection/baby.svg";
+import questionMark from "../assets/svgs/detection/questionMark.svg";
+import sleepy from "../assets/svgs/detection/sleepy.svg";
+import hungry from "../assets/svgs/detection/hungry.svg";
+import uncomfortable from "../assets/svgs/detection/uncomfortable.svg";
+import sick from "../assets/svgs/detection/sick.svg";
+import burp from "../assets/svgs/detection/burp.svg";
+
 export const DETECTION = {
   NORMAL: {
-    ICON: "baby",
-    MESSAGE: "아기가 평온한 상태에요",
+    ICON: baby,
+    MESSAGE: "평온한 ",
     COLOR: "#7BB7F7",
   },
   LOADING: {
-    ICON: "questionMark",
+    ICON: questionMark,
     COLOR: "#FFA6AF",
   },
   SLEEPY: {
-    ICON: "sleepy",
-    MESSAGE: "아기가 졸려서 울고 있어요",
+    ICON: sleepy,
+    MESSAGE: "졸려서 ",
     SOLUTION: "5분 가량 안고 걸어주세요",
     COLOR: "#E3A6FF",
   },
   HUNGRY: {
-    ICON: "hungry",
-    MESSAGE: "아기가 배고파서 울고 있어요",
+    ICON: hungry,
+    MESSAGE: "배고파서 ",
     SOLUTION: "분유를 먹여주세요",
     COLOR: "#FAD92E",
   },
   UNCOMFORTABLE: {
-    ICON: "uncomfortable",
-    MESSAGE: "아기가 불편해서 울고 있어요",
+    ICON: uncomfortable,
+    MESSAGE: "불편해서 ",
     SOLUTION: "아기 자세를 바로해주세요",
     COLOR: "#FF9447",
   },
   SICK: {
-    ICON: "sick",
-    MESSAGE: "아기가 아펏 울고 있어 요",
+    ICON: sick,
+    MESSAGE: "아파서 ",
     SOLUTION: "아기를 데리고 병원에 가세요",
     COLOR: "#FF5D5D",
   },
   BURT: {
-    ICON: "burp",
-    MESSAGE: "아기가 트림하고 싶어서 울고 있어요",
+    ICON: burp,
+    MESSAGE: "트림하고 싶어서 ",
     SOLUTION: "아기를 트림시켜주세요",
     COLOR: "#4BF25C",
   },
