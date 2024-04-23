d10efa9f5c4d15e2ce5071f9bdfb7bcab3b1feb1
이도훈 <nestour95@gmail.com>
2024-04-23 15:43:48 +0900

    ✨feat: Gateway에 인가 필터 추가

---
+++ back/api-gateway/src/main/java/ssafy/icon/apigateway/auth/jwt/JwtProps.java
@@ -0,0 +1,12 @@
+package ssafy.icon.apigateway.auth.jwt;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Getter
+@AllArgsConstructor
+@ConfigurationProperties(prefix = "jwt")
+public class JwtProps {
+	private String accessKey;
+}
