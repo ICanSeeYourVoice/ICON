9095b8360a00805c05b0d67416e162235616c87e
nayeon <kknyapple@gmail.com>
2024-04-23 12:18:28 +0900

    feat: Nav 컴포넌트 구현

---
+++ front/src/components/common/Navigator/Nav.tsx
@@ -0,0 +1,61 @@
+import { useNavigate } from "react-router-dom";
+import setting from "../../../assets/svgs/nav/setting.svg";
+import detection from "../../../assets/svgs/nav/detection.svg";
+import diary from "../../../assets/svgs/nav/diary.svg";
+import chat from "../../../assets/svgs/nav/chat.svg";
+
+interface NavItemProps {
+  text: string;
+  icon: string;
+  path: string;
+}
+
+const NavItem = ({
+  text,
+  icon,
+  path,
+}: {
+  text: string;
+  icon: string;
+  path: string;
+}) => {
+  const navigate = useNavigate();
+
+  const handleNavItemClick = () => {
+    navigate(path);
+  };
+
+  return (
+    <div
+      onClick={handleNavItemClick}
+      className="flex flex-col justify-center items-center gap-1"
+    >
+      <img src={icon} alt="" />
+      <span className="text-xs text-gray-0">{text}</span>
+    </div>
+  );
+};
+
+const Nav = () => {
+  const navList: NavItemProps[] = [
+    { text: "설정", icon: setting, path: "/setting" },
+    { text: "울음감지", icon: detection, path: "/" },
+    { text: "성장일지", icon: diary, path: "/record" },
+    { text: "상담", icon: chat, path: "/chat" },
+  ];
+
+  return (
+    <nav className="bg-white w-full h-[3.3125rem] flex justify-around items-center px-4 shadow-nav fixed bottom-0 left-0 z-10">
+      {navList.map((item, index) => (
+        <NavItem
+          key={index}
+          text={item.text}
+          icon={item.icon}
+          path={item.path}
+        />
+      ))}
+    </nav>
+  );
+};
+
+export default Nav;
