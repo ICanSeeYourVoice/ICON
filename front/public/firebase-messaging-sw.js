43faa2b2edac1745c36385244ffac2033b6254a7
nayeon <kknyapple@gmail.com>
2024-04-24 17:31:55 +0900

    feat: firebase service worker 설정
    백그라운드 상태일 때 메세지 처리

---
+++ front/public/firebase-messaging-sw.js
@@ -0,0 +1,34 @@
+importScripts(
+  "https://www.gstatic.com/firebasejs/10.11.0/firebase-app-compat.js"
+);
+importScripts(
+  "https://www.gstatic.com/firebasejs/10.11.0/firebase-messaging-compat.js"
+);
+
+self.addEventListener("install", function (e) {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function (e) {
+  console.log("fcm service worker가 실행되었습니다.");
+});
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCwSXSJ9ZEHm8jJ1MKtICg4ujZjAYl2dUc",
+  projectId: "icon-e9294",
+  messagingSenderId: "233502549164",
+  appId: "1:233502549164:web:e8c61860bf98071a756b09",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  const notificationTitle = payload.title;
+  const notificationOptions = {
+    body: payload.body,
+    // icon: payload.icon
+  };
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
