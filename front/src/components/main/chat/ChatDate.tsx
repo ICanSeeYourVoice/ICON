e02d9e8b1e488583344d0e7253bb60e778d35280
심우석 <dntjr5741@naver.com>
2024-04-24 23:08:21 +0900

    feat: Chat 페이지 라우터 설정

--- front/src/components/main/chat/ChatDate.tsx
+++ front/src/components/main/chat/ChatDate.tsx
@@ -14,10 +14,12 @@ const DateDisplay: React.FC<DateDisplayProps> = ({ date }) => {
   };
 
   return (
-    <div className="w-[6rem] h-[1rem] relative">
-      <div className="w-[5rem] h-[1rem] left-0 top-0 absolute bg-zinc-100 rounded-[1rem]" />
-      <div className="tracking-wider left-[0.5rem] top-[0.2rem] absolute text-center text-slate-500 text-[0.48rem] ">
-        {formatDate(date)}
+    <div className="flex justify-center">
+      <div className="w-[6rem] h-[1rem] relative">
+        <div className="w-[5rem] h-[1rem] left-0 top-0 absolute bg-zinc-100 rounded-[1rem]" />
+        <div className="tracking-wider left-[0.5rem] top-[0.2rem] absolute text-center text-slate-500 text-[0.48rem] ">
+          {formatDate(date)}
+        </div>
       </div>
     </div>
   );
