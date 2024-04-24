18d35c136e8431a6cac03fb0391245ed294ca328
nayeon <kknyapple@gmail.com>
2024-04-24 22:59:32 +0900

    refactor: fcm 알림 함수 수정
    setNotification 매개변수 설정, export 가능하게 변경

--- front/src/service/foregroundMessage.ts
+++ front/src/service/foregroundMessage.ts
@@ -3,13 +3,24 @@ import { app } from "./initFirebase";
 
 const messaging = getMessaging(app);
 
-onMessage(messaging, (payload: any) => {
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body,
-  };
+export const messageHandler = (
+  setNotification: React.Dispatch<
+    React.SetStateAction<{ title: string; body: string }>
+  >
+) => {
+  onMessage(messaging, (payload: any) => {
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+      body: payload.notification.body,
+    };
 
-  if (Notification.permission === "granted") {
-    new Notification(notificationTitle, notificationOptions);
-  }
-});
+    setNotification({
+      title: payload?.notification?.title,
+      body: payload?.notification?.body,
+    });
+
+    if (Notification.permission === "granted") {
+      new Notification(notificationTitle, notificationOptions);
+    }
+  });
+};
