fd5ef41e69cd00b565cacbf060f72d6c16bf7b0c
nayeon <kknyapple@gmail.com>
2024-04-25 17:58:41 +0900

    style: layout css 수정

--- front/src/components/common/layout/DarkCommonLayout.tsx
+++ front/src/components/common/layout/DarkCommonLayout.tsx
@@ -3,7 +3,7 @@ import Nav from "../Navigator/Nav";
 
 const DarkCommonLayout = ({ title }: { title: string }) => {
   return (
-    <div className="flex flex-col items-center bg-dark h-screen w-screen pt-[3.125rem] pb-[10.3125rem] gap-[4.75rem] ">
+    <div className="flex flex-col items-center bg-dark h-screen w-screen pt-[3.125rem] pb-[10.3125rem] gap-[4.5rem] ">
       <header className="text-primary w-[15.25rem] text-2xl">{title}</header>
       <main className="flex flex-col items-center justify-center w-full">
         <Outlet />
