f78d2a1688ae8916b4c8c6577c954b0f9674c950
이도훈 <nestour95@gmail.com>
2024-04-26 10:59:50 +0900

    ✨feat: 스마트싱스 토큰 등록 API 구현

---
+++ back/common-service/src/main/java/ssafy/icon/commonservice/domain/smartthings/client/SmartthingsApiClient.java
@@ -0,0 +1,16 @@
+package ssafy.icon.commonservice.domain.smartthings.client;
+
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import ssafy.icon.commonservice.domain.smartthings.client.dto.GetDevicesApiResponse;
+
+@FeignClient(name = "smartthings-api", url = "${external-api.smartthings.url}")
+public interface SmartthingsApiClient {
+
+	@GetMapping("/devices")
+	GetDevicesApiResponse getDevices(
+		@RequestHeader("Authorization") String smartthingsToken
+	);
+}
---
+++ back/common-service/src/main/java/ssafy/icon/commonservice/domain/smartthings/entity/SmartthingsToken.java
@@ -0,0 +1,43 @@
+package ssafy.icon.commonservice.domain.smartthings.entity;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
+
+import com.fasterxml.jackson.databind.ser.Serializers.Base;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.NoArgsConstructor;
+import ssafy.icon.commonservice.domain.member.entity.Member;
+import ssafy.icon.commonservice.global.entity.BaseEntity;
+
+@NoArgsConstructor(access = PROTECTED)
+@Entity
+public class SmartthingsToken extends BaseEntity {
+
+	@GeneratedValue(strategy = IDENTITY)
+	@Column(name = "smartthings_token_id")
+	@Id
+	private Integer id;
+
+	@OneToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(nullable = false , unique = true, name = "member_id")
+	private Member member;
+
+	@Column(nullable = false, unique = true)
+	private String token;
+
+	public SmartthingsToken(Member member, String token) {
+		this.member = member;
+		this.token = token;
+	}
+
+	public void changeToken(String token) {
+		this.token = token;
+	}
+
+}
