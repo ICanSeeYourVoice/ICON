cc78b2ad11f1244a279e77a4daab651e4c1f4b12
nayeon <kknyapple@gmail.com>
2024-04-26 10:49:48 +0900

    feat: 아기울음감지 모든 페이지에서 실행, 오디오 처음에만 설정
    App.tsx 아기울음감지 설정, ref 이용해 오디오 실행 여부 저장

--- front/src/pages/detection/DetectionPage.tsx
+++ front/src/pages/detection/DetectionPage.tsx
@@ -5,68 +5,14 @@ import {
   DETECTION_INFO,
   LOADING_INFO,
 } from "../../constants/detection";
-import * as tf from "@tensorflow/tfjs";
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 import { useDetectionStore } from "../../stores/detection";
-import {
-  loadYamnetModel,
-  processAudioData,
-} from "../../utils/cryingClassification";
 import { useNavigate } from "react-router";
-import toast from "react-hot-toast";
 
 const DetectionPage = () => {
   const navigate = useNavigate();
-
-  const model = useRef<tf.GraphModel | null>(null);
   const isBabyCry = useDetectionStore((state: any) => state.isBabyCry);
-  const setIsBabyCry = useDetectionStore((state: any) => state.setIsBabyCry);
-
   const cryingType = useDetectionStore((state: any) => state.cryingType);
-  const setCryingType = useDetectionStore((state: any) => state.setCryingType);
-
-  useEffect(() => {
-    const fetchDataAndProcess = async () => {
-      let toastId;
-
-      try {
-        toastId = toast.loading("아기울음감지를 준비 중입니다.");
-
-        const yamnet = await loadYamnetModel();
-        model.current = yamnet;
-        const constraints = { audio: true };
-
-        const stream = await navigator.mediaDevices.getUserMedia(constraints);
-
-        const audioCtx = new AudioContext({ sampleRate: 16000 });
-        const source = audioCtx.createMediaStreamSource(stream);
-        const scriptNode = audioCtx.createScriptProcessor(8192, 1, 1);
-
-        scriptNode.onaudioprocess = processAudioData(
-          yamnet,
-          setIsBabyCry,
-          setCryingType
-        );
-
-        source.connect(scriptNode);
-        scriptNode.connect(audioCtx.destination);
-
-        toast.success("울음감지 시작");
-      } catch (error) {
-        toast.error("아기울음감지를 준비 중 오류가 발생했습니다.");
-      } finally {
-        toast.dismiss(toastId);
-      }
-    };
-
-    if (!isBabyCry) fetchDataAndProcess();
-
-    return () => {
-      if (model.current) {
-        model.current.dispose();
-      }
-    };
-  }, []);
 
   useEffect(() => {
     if (cryingType !== 0) {
