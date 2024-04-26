a48c50868b7096d5b1a24b6a089c4f9eccbff184
nayeon <kknyapple@gmail.com>
2024-04-26 13:15:52 +0900

    feat: toast css 수정 및 duration 설정

--- front/src/pages/setting/notification/QRScanPage.tsx
+++ front/src/pages/setting/notification/QRScanPage.tsx
@@ -42,34 +42,39 @@ const QRScanPage = () => {
     if (data?.userId && !isScanned) {
       setIsScanned(true);
 
-      toast((t) => (
-        <div className="flex flex-col items-center justify-center w-full">
-          <p>{data.userId}(을)를 보호자로 등록하시겠습니까?</p>
-
-          <div className="mt-4 flex text-white">
-            <button
-              className="bg-green-500 py-2 px-4 rounded mr-2"
-              onClick={() => {
-                toast.success(`${data.userId}를 보호자 목록에 추가하였습니다.`);
-                setData(null);
-                navigate("/setting/share");
-                toast.dismiss(t.id);
-              }}
-            >
-              확인
-            </button>
-            <button
-              className="bg-red-500 py-2 px-4 rounded"
-              onClick={() => {
-                toast.dismiss(t.id);
-                setIsScanned(false);
-              }}
-            >
-              취소
-            </button>
+      toast(
+        (t) => (
+          <div className="flex flex-col items-center justify-center w-full">
+            <p>{data.userId}(을)를 보호자로 등록하시겠습니까?</p>
+            <hr />
+            <div className="mt-4 flex w-full justify-end text-white">
+              <button
+                className="bg-gray-1 py-2 px-4 rounded mr-[0.4rem]"
+                onClick={() => {
+                  toast.dismiss(t.id);
+                  setIsScanned(false);
+                }}
+              >
+                취소
+              </button>
+              <button
+                className="bg-primary py-2 px-4 rounded mr-2"
+                onClick={() => {
+                  toast.success(
+                    `${data.userId}를 보호자 목록에 추가하였습니다.`
+                  );
+                  setData(null);
+                  navigate("/setting/share");
+                  toast.dismiss(t.id);
+                }}
+              >
+                확인
+              </button>
+            </div>
           </div>
-        </div>
-      ));
+        ),
+        { duration: Infinity }
+      );
     }
   }, [data]);
 
