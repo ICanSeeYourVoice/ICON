78969eff61106c76d6609095b1236fc4f2e01be4
심우석 <dntjr5741@naver.com>
2024-04-28 21:19:42 +0900

    feat: Voice 페이지 css 초안

--- front/src/components/main/voice/ClovaVoice.tsx
+++ front/src/components/main/voice/ClovaVoice.tsx
@@ -1,8 +1,11 @@
 import { useMutation } from "@tanstack/react-query";
 import { clovaVoice } from "../../../apis/Voice";
 import { useState, useRef } from "react";
+import mic from "../../../assets/svgs/voice/mic.svg";
 
 const ClovaVoice = () => {
+  const [text, setText] = useState("");
+
   const { mutate } = useMutation({
     mutationFn: clovaVoice,
     onSuccess: (data) => {
@@ -16,7 +19,6 @@ const ClovaVoice = () => {
     },
   });
 
-  const [text, setText] = useState("");
   const audioRef = useRef<HTMLAudioElement>(null);
 
   const handleTextChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -37,10 +39,30 @@ const ClovaVoice = () => {
 
   return (
     <div>
-      <h1>TTS</h1>
-      <input type="text" value={text} onChange={handleTextChange} />
-      <button onClick={handleSubmit}>Generate Voice</button>
-      <audio ref={audioRef} controls />
+      <div className="bg-blue-800 rounded-full p-2 inline-block w-16 h-16 flex items-center justify-center">
+        <img src={mic} alt="mic" className="w-full h-full object-cover" />
+      </div>
+
+      <div className="fixed bottom-0 left-0 right-0 p-3 bg-white shadow-xl">
+        <audio ref={audioRef} controls />
+        <div className="flex justify-center">
+          <input
+            type="text"
+            className="flex-1 p-2"
+            placeholder="아기에게 말해보세요"
+            value={text}
+            onChange={handleTextChange}
+          />
+          <button
+            onClick={handleSubmit}
+            className={`ml-2 w-[2rem] h-[2rem] flex items-center justify-center rounded-full p-2 ${
+              text ? "bg-blue-500" : "bg-blue-300"
+            }`}
+          >
+            <img src={mic} alt="Send" />
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
