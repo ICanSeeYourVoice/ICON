d7c61399962b09ef1d09a953322b143d78ea2021
심우석 <dntjr5741@naver.com>
2024-04-29 23:31:07 +0900

    feat: wavesurfer.js 라이브러리 추가 TTS 기능 구현

--- front/src/components/common/layout/VoiceLayout.tsx
+++ front/src/components/common/layout/VoiceLayout.tsx
@@ -1,8 +1,8 @@
 import { Outlet } from "react-router-dom";
 const RecordLayout = () => {
   return (
-    <div className="flex flex-col items-center w-screen pt-[3.125rem] gap-[4.5rem] ">
-      <header className="text-primary w-[15.25rem] text-2xl">TTS</header>
+    <div className="flex flex-col items-center w-screen pt-[3.125rem] gap-[4.5rem]  ">
+      <header className="text-primary w-[15.25rem] text-2xl ">TTS</header>
       <main className="flex flex-col items-center justify-center w-full">
         <Outlet />
       </main>
