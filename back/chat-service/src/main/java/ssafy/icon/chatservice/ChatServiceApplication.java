955d0b15e2b0115d49257b017204cf19a02f8003
Dohun Lee <nestour95@gmail.com>
2024-04-29 16:32:15 +0900

    🚀hotfix: 챗봇 서비스에 유레카 설정 추가

--- back/chat-service/src/main/java/ssafy/icon/chatservice/ChatServiceApplication.java
+++ back/chat-service/src/main/java/ssafy/icon/chatservice/ChatServiceApplication.java
@@ -2,7 +2,9 @@ package ssafy.icon.chatservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ChatServiceApplication {
 
