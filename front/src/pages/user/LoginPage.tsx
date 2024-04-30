81a5f0e24feedbc2cc0e85bcd61ed9032196298d
심우석 <dntjr5741@naver.com>
2024-04-30 14:05:46 +0900

    feat: 로그인 API 연결

--- front/src/pages/user/LoginPage.tsx
+++ front/src/pages/user/LoginPage.tsx
@@ -4,12 +4,12 @@ import { useNavigate } from "react-router";
 import CommonInput from "../../components/common/Input/CommonInput";
 import PasswordInput from "../../components/common/Input/PasswordInput";
 import { useState } from "react";
+import { useMutation } from "@tanstack/react-query";
+import { userLogin } from "../../apis/User";
 
 const LoginPage = () => {
   const navigate = useNavigate();
-  const handleButtonClick = () => {
-    navigate("/detection");
-  };
+
   const handleJoinClick = () => {
     navigate("/join");
   };
@@ -22,6 +22,26 @@ const LoginPage = () => {
   };
   const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInputPasswordValue(e.target.value);
+  };
+
+  const { mutate } = useMutation({
+    mutationFn: userLogin,
+    onSuccess: (res) => {
+      if (res.access_token) {
+        sessionStorage.setItem("access_Token", res.access_token);
+        navigate("/detection");
+      } else {
+        alert("토큰이 존재하지 않습니다. 다시 시도하세요.");
+      }
+    },
+    onError: (error) => {
+      alert("아이디나 비밀번호를 다시 확인해 주세요");
+      console.log("로그인실패", error);
+    },
+  });
+
+  const handleConfirmClick = () => {
+    mutate({ uid: inputIdValue, pw: inputPasswordValue });
   };
 
   return (
@@ -40,7 +60,7 @@ const LoginPage = () => {
       <div className="flex gap-[0.5rem] text-[11px] justify-end w-[15.25rem]">
         <div onClick={handleJoinClick}>회원가입</div>
       </div>
-      <PostButton label="로그인" onClick={handleButtonClick} />
+      <PostButton label="로그인" onClick={handleConfirmClick} />
     </div>
   );
 };
