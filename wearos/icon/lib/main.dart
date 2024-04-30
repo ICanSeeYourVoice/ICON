bb84bdf2d18524c29c2105d55131b42bcaf164bd
탁하윤 <thagyun@naver.com>
2024-04-30 13:08:04 +0900

    ✨feat: 프로젝트, fcm, BLE 설정

---
+++ wearos/icon/lib/main.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:icon/home_controller.dart';
+import 'package:icon/home_view.dart';
+import 'package:get/get.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+// background message
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  print('background message ${message.notification!.body}');
+}
+
+void initializeNotification() async {
+  // background message notification
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+  final flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+  await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<
+          AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(const AndroidNotificationChannel(
+          'high_importance_channel', 'high_importance_notification',
+          importance: Importance.max));
+
+  await flutterLocalNotificationsPlugin.initialize(const InitializationSettings(
+    android: AndroidInitializationSettings("@mipmap/ic_launcher"),
+  ));
+
+  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
+}
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  initializeNotification();
+
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    RemoteNotification? notification = message.notification;
+
+    if (notification != null) {
+      FlutterLocalNotificationsPlugin().show(
+        notification.hashCode,
+        notification.title,
+        notification.body,
+        const NotificationDetails(
+            android: AndroidNotificationDetails(
+          'high_importance_channel',
+          'high_importance_notification',
+          importance: Importance.max,
+        )),
+      );
+      String messageString = message.notification!.body!;
+      print('Foreground 메시지 수신: $messageString');
+    }
+  });
+  // // background message notification
+  // FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  //
+  // await FirebaseMessaging.instance.setAutoInitEnabled(true);
+  // final fcmToken = await FirebaseMessaging.instance.getToken();
+  // print("FCMToken $fcmToken");
+  //
+  // FirebaseMessaging.onMessage.listen((RemoteMessage event) {
+  //   print("message recieved");
+  //   print(event.notification!.body);
+  // });
+  // FirebaseMessaging.onMessageOpenedApp.listen((message) {
+  //   print('Message clicked!');
+  // });
+  Get.put(HomeController());
+  runApp(const GetMaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomeView(),
+  ));
+}
