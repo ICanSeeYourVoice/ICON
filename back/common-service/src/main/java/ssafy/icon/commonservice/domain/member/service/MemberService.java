c4eac48ae2da5b876d40c9213208ed0e7a159612
김현영 <hggew@naver.com>
2024-05-01 21:42:52 +0900

    feat: back-common token 저장 및 수정 api

--- back/common-service/src/main/java/ssafy/icon/commonservice/domain/member/service/MemberService.java
+++ back/common-service/src/main/java/ssafy/icon/commonservice/domain/member/service/MemberService.java
@@ -5,6 +5,8 @@ import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import ssafy.icon.commonservice.domain.member.dto.AddTokenReq;
 import ssafy.icon.commonservice.domain.member.dto.SignUpForm;
 import ssafy.icon.commonservice.domain.member.entity.Member;
 import ssafy.icon.commonservice.domain.member.repository.MemberRepository;
@@ -28,5 +30,16 @@ public class MemberService {
 			.pw(passwordEncoder.encode(form.pw()))
 			.name(form.name())
 			.build());
+	}
+
+	public void addToken(Integer memberId, AddTokenReq token) {
+		// token의 id로 유저 정보 가져오기
+		Member member = memberRepository.findById(memberId)
+			.orElseThrow(() -> new MemberException(BAD_REQUEST, "존재하지 않은 회원 ID입니다."));
+
+		// new token 저장
+		member.updateToken(token.getToken(), token.isApp());
+
+		memberRepository.save(member);
 	}
 }
