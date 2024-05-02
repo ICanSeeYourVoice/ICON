2bd2aa41329e64316e5733a357a9ebcc261c8436
이도훈 <nestour95@gmail.com>
2024-05-02 10:35:39 +0900

    ✨feat: 루틴 트리거명 변경

--- back/common-service/src/main/java/ssafy/icon/commonservice/domain/smartthings/enums/RoutineTrigger.java
+++ back/common-service/src/main/java/ssafy/icon/commonservice/domain/smartthings/enums/RoutineTrigger.java
@@ -1,5 +1,5 @@
 package ssafy.icon.commonservice.domain.smartthings.enums;
 
 public enum RoutineTrigger {
-	SLEEP, HUNGRY, UNCOMFORTABLE, SICK, BURP, DANGER
+	CRY, TIRED, HUNGRY, DISCOMFORT, DANGER, PAIN
 }
