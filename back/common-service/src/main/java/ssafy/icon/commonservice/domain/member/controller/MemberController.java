2d850552e6d33f577d217071037b6d70e8688895
김현영 <hggew@naver.com>
2024-05-03 21:13:38 +0900

    feat: 보호자 삭제api url 수정

--- back/common-service/src/main/java/ssafy/icon/commonservice/domain/member/controller/MemberController.java
+++ back/common-service/src/main/java/ssafy/icon/commonservice/domain/member/controller/MemberController.java
@@ -19,6 +19,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -78,9 +79,9 @@ public class MemberController {
 		return ResponseEntity.ok().build();
 	}
 
-	@DeleteMapping("/guardian")
+	@DeleteMapping("/guardian/{guardianId}")
 	public ResponseEntity<Void> deleteGuardian(@RequestHeader("X-Authorization-Id") Integer memberId,
-		@RequestBody Integer guardianId) {
+		@PathVariable Integer guardianId) {
 		memberService.deleteGuardian(memberId, guardianId);
 		return ResponseEntity.ok().build();
 	}
