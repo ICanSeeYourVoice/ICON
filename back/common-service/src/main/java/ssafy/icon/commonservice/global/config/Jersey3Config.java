8bd37d8465a1f82752c3e7e728f4ddb64bb04fe9
이도훈 <nestour95@gmail.com>
2024-05-06 21:16:39 +0900

    ✨feat: 성장일지 생성 API 구현

--- back/common-service/src/main/java/ssafy/icon/commonservice/global/config/Jersey3Config.java
+++ back/common-service/src/main/java/ssafy/icon/commonservice/global/config/Jersey3Config.java
@@ -1,6 +1,10 @@
 package ssafy.icon.commonservice.global.config;
 
+import com.netflix.discovery.AbstractDiscoveryClientOptionalArgs;
+import com.netflix.discovery.Jersey3DiscoveryClientOptionalArgs;
+import com.netflix.discovery.shared.transport.jersey.TransportClientFactories;
 import com.netflix.discovery.shared.transport.jersey3.Jersey3TransportClientFactories;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,9 +12,15 @@ import org.springframework.context.annotation.Configuration;
 public class Jersey3Config {
 
 	@Bean
-	public Jersey3TransportClientFactories jersey3TransportClientFactories() {
-		return new Jersey3TransportClientFactories();
+	@ConditionalOnMissingBean(AbstractDiscoveryClientOptionalArgs.class)
+	public Jersey3DiscoveryClientOptionalArgs jersey3DiscoveryClientOptionalArgs() {
+		return new Jersey3DiscoveryClientOptionalArgs();
 	}
 
+	@Bean
+	@ConditionalOnMissingBean(TransportClientFactories.class)
+	public Jersey3TransportClientFactories jersey3TransportClientFactories() {
+		return Jersey3TransportClientFactories.getInstance();
+	}
 
 }
