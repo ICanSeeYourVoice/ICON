4a979db21e3b909196becdc831d9da807ed9a8ee
nayeon <kknyapple@gmail.com>
2024-05-06 20:53:41 +0900

    style: 레이아웃 css 변경

--- front/src/pages/record/RecordPage.tsx
+++ front/src/pages/record/RecordPage.tsx
@@ -4,10 +4,15 @@ import TodayDiary from "../../components/main/record/TodayDiary";
 
 const RecordPage = () => {
   return (
-    <div className="flex flex-col items-center h-screen w-screen pt-[3.125rem] ">
-      <header className="text-primary w-[15.25rem] text-2xl">성장일지</header>
-      <TodayDiary />
-      <TodayChart />
+    <div className="flex flex-col items-center h-screen w-screen">
+      <header className=" bg-white flex items-center justify-center text-primary h-[6rem] w-full text-2xl fixed">
+        <p className="w-[80%]">성장일지</p>
+      </header>
+      <main className="flex flex-col items-center justify-center w-full mt-[6rem]">
+        <TodayDiary />
+        <TodayChart />
+      </main>
+
       <Nav />
     </div>
   );
