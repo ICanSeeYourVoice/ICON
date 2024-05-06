4c1853139781d42e34a20dd2b54ed6aab907fd48
nayeon <kknyapple@gmail.com>
2024-05-06 18:25:23 +0900

    style: 스마트싱스 페이지 css 변경

--- front/src/pages/setting/smartThings/SmartThingsPage.tsx
+++ front/src/pages/setting/smartThings/SmartThingsPage.tsx
@@ -32,7 +32,7 @@ const SmartThingsPage = () => {
   });
 
   return (
-    <div className="flex flex-col items-center h-screen w-screen pt-[3.125rem] pb-[10.3125rem] gap-[1.5rem] ">
+    <div className="flex flex-col items-center h-screen w-screen">
       <TopBar text="SmartThings" path="setting" />
       <main className="flex flex-col items-center justify-center w-full flex-1">
         {isLodding ? (
