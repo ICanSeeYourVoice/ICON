b85fe031342b5cda8ba690987d0261f0fa59a920
Dohun Lee <nestour95@gmail.com>
2024-05-07 09:21:58 +0900

    ✨feat: 성장일지 삭제 API 구현

--- back/common-service/src/main/java/ssafy/icon/commonservice/domain/diary/controller/DiaryController.java
+++ back/common-service/src/main/java/ssafy/icon/commonservice/domain/diary/controller/DiaryController.java
@@ -1,13 +1,16 @@
 package ssafy.icon.commonservice.domain.diary.controller;
 
+import java.time.LocalDate;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ssafy.icon.commonservice.domain.diary.dto.DiaryRegisterForm;
 import ssafy.icon.commonservice.domain.diary.service.DiaryService;
@@ -25,6 +28,15 @@ public class DiaryController {
 		@RequestBody DiaryRegisterForm form
 	) {
 		diaryService.post(memberId, form);
+		return ResponseEntity.ok().build();
+	}
+
+	@DeleteMapping
+	public ResponseEntity<Void> delete(
+		@RequestHeader("X-Authorization-Id") Integer memberId,
+		@RequestParam("date") LocalDate date
+	) {
+		diaryService.delete(memberId, date);
 		return ResponseEntity.ok().build();
 	}
 
