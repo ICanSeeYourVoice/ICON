22d8b32722e25a0b374213d0636c9c69a25f202e
김민석 <kms278711@naver.com>
2024-05-07 14:34:23 +0900

    🐛 fix : 통계 상세조회 API 날짜 형식 변경

--- back/analyze-service/src/main/java/ssafy/icon/analyzeservice/domain/analyze/dto/StatisticsTimeline.java
+++ back/analyze-service/src/main/java/ssafy/icon/analyzeservice/domain/analyze/dto/StatisticsTimeline.java
@@ -1,9 +1,7 @@
 package ssafy.icon.analyzeservice.domain.analyze.dto;
 
-import java.time.LocalDateTime;
-
 public record StatisticsTimeline(
-		String type,
-		LocalDateTime cryingDate
+	String type,
+	String cryingDate
 ) {
 }
