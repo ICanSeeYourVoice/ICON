f6f11147e490a5cc0c32ff855dcf54fc80a3564c
탁하윤 <thagyun@naver.com>
2024-05-08 22:25:03 +0900

    fix: resolve web ble type error

--- front/src/pages/setting/ble/BleConnect.tsx
+++ front/src/pages/setting/ble/BleConnect.tsx
@@ -1,19 +1,16 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import toast from "react-hot-toast";
 import SmallButton from "../../../components/common/button/SmallButton";
 import useBleStore from "../../../stores/bluetooth";
+import BleImage from "../../../components/main/setting/ble/BleImage";
+
 type DeviceOptions = {
   bleService: BluetoothServiceUUID;
   bleCharacteristic: BluetoothCharacteristicUUID;
 };
+
 interface BleStore {
-  isConnected: boolean;
   device: BluetoothDevice | null;
-  server: BluetoothRemoteGATTServer | undefined;
-  service: BluetoothRemoteGATTService | undefined;
-  characteristic: BluetoothRemoteGATTCharacteristic | undefined;
-  deviceValue: string;
-  changeValue: string;
   setDevice: (device: BluetoothDevice) => void;
   setServer: (server: BluetoothRemoteGATTServer | undefined) => void;
   setService: (service: BluetoothRemoteGATTService | undefined) => void;
@@ -25,31 +22,29 @@ interface BleStore {
   setChangeValue: (value: string) => void;
   writeCharacteristic: (value: string) => void;
 }
+
 const BleConnect = () => {
   const {
-    changeValue,
-    characteristic,
+    device,
     setDevice,
     setServer,
     setService,
     setCharacteristic,
-    setChangeValue,
     writeCharacteristic,
   } = useBleStore() as BleStore;
-  const [deviceValue, setDeviceValue] = useState(""); // 처음 특성의 value 값
-  const [testValue, setTestValue] = useState(""); // 변경된 특성 value 응답 값
-  const characteristicGlobal = useRef(
-    null as BluetoothRemoteGATTCharacteristic | null
-  );
+  const [connection, setConnection] = useState(false);
 
   useEffect(() => {
     if (!("bluetooth" in navigator)) {
-      console.error("블루투스를 지원하지 않는 브라우저입니다.");
-      toast.error("블루투스를 지원하지 않는 브라우저입니다.");
+      toast.error(
+        "블루투스를 지원하지 않는 브라우저입니다.\nChrome에서 이용해주세요.",
+        { duration: 5000 }
+      );
       return;
     }
     return () => {};
   }, []);
+
   const connectToDevice = async ({
     bleService,
     bleCharacteristic,
@@ -72,58 +67,13 @@ const BleConnect = () => {
         bleCharacteristic
       );
       console.log("characteristic", characteristic);
-
-      // characteristicGlobal.current = characteristic ?? null;
       setCharacteristic(characteristic);
-      // characteristic?.startNotifications().then(
-      //   () => {
-      //     console.log("Notifications started");
-      //     characteristic?.addEventListener(
-      //       "characteristicvaluechanged",
-      //       handleCharacteristicValueChanged
-      //     );
-      //   },
-      //   (error) => {
-      //     console.error("Failed to start notifications", error);
-      //   }
-      // );
-
-      // const handleCharacteristicValueChanged = (event: Event) => {
-      //   const characteristic =
-      //     event.target as BluetoothRemoteGATTCharacteristic;
-      //   console.log("Characteristic value changed", characteristic.uuid);
-      //   if (characteristic.value) {
-      //     const value = new TextDecoder().decode(characteristic.value);
-      //     console.log("New value", value);
-      //     setTestValue(value);
-      //     setChangeValue(value);
-      //   } else {
-      //     console.error("Characteristic value is null");
-      //   }
-      // };
-
-      const valueDataView = await characteristic?.readValue();
-      const valueString = new TextDecoder().decode(valueDataView);
-      setDeviceValue(valueString);
+      setConnection(true);
+      writeCharacteristic("init");
     } catch (err) {
       console.error(err);
     }
   };
-
-  // const writeCharacteristic = async (value: string) => {
-  //   if (!characteristicGlobal.current) {
-  //     console.error("Characteristic is not available.");
-  //     return;
-  //   }
-  //   try {
-  //     const encoder = new TextEncoder();
-  //     await characteristicGlobal.current.writeValue(encoder.encode(value));
-  //     await characteristic?.writeValue(encoder.encode(value));
-  //     console.log("Value written to the characteristic:", value);
-  //   } catch (error) {
-  //     console.error("Failed to write value", error);
-  //   }
-  // };
 
   const handleScanClick = () => {
     connectToDevice({
@@ -132,16 +82,25 @@ const BleConnect = () => {
     });
   };
 
+  const handleDisconnectClick = () => {
+    if (!device) {
+      return;
+    }
+    if (device.gatt?.connected) {
+      device.gatt.disconnect;
+      setConnection(false);
+    }
+  };
+
   return (
     <div className="flex flex-col items-center mb-[1rem] gap-[1rem]">
       <p>워치랑 연결하고 알림을 받아보세요.</p>
-      <SmallButton label="연결" onClick={handleScanClick} />
-      <SmallButton
-        label="워치 알림 테스트"
-        onClick={() => writeCharacteristic("hungry")}
-      />
-      {changeValue != "" ? <p>변경 값: {changeValue}</p> : null}
-      {deviceValue != "" ? <p>초기 값: {deviceValue}</p> : null}
+      <BleImage />
+      {!connection ? (
+        <SmallButton label="연결" onClick={handleScanClick} />
+      ) : (
+        <SmallButton label="연결해제" onClick={handleDisconnectClick} />
+      )}
     </div>
   );
 };
