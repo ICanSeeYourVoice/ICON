5b7189c3c51ed7a628b254256f520067e95ca606
김현영 <hggew@naver.com>
2024-05-09 10:51:41 +0900

    feat: face-api 얼굴 1개 감지 변경

--- front/src/pages/detection/PosePage.tsx
+++ front/src/pages/detection/PosePage.tsx
@@ -1,12 +1,23 @@
+import { useState } from 'react';
 import { FaceRecognizerCam } from '../../utils/faceRecognition';
 
 const PosePage = () => { 
  
-
+    const [features, setFeatures] = useState({
+        hasLeftEyes: false,
+        hasRightEyes: false,
+        hasNose: false,
+        hasMouth: false,
+      });
+    
     return (
         <div>
             <h1>Other Page</h1>
-            <FaceRecognizerCam  /> 
+            <FaceRecognizerCam featuresDetected={features} setFeaturesDetected={setFeatures} />
+            <p>Has Left eyes: {features.hasLeftEyes.toString()}</p>
+            <p>Has Right eyes: {features.hasRightEyes.toString()}</p>
+            <p>Has nose: {features.hasNose.toString()}</p>
+            <p>Has mouth: {features.hasMouth.toString()}</p>
         </div>
     );
 };
--- front/src/utils/faceRecognition.tsx
+++ front/src/utils/faceRecognition.tsx
@@ -2,43 +2,43 @@ import { useEffect, useRef, useState } from "react";
 import { toast } from "react-hot-toast";
 // import tf from '@tensorflow/tfjs-node';
 import * as faceapi from "@vladmandic/face-api";
-// console.log(faceapi.nets);
 
-const FaceRecognizerCam = () => {
+const FaceRecognizerCam = ({ featuresDetected, setFeaturesDetected }) => {
   const videoRef = useRef<HTMLVideoElement | null>(null);
   const camMediaStream = useRef<MediaStream | null>(null);
   const intervalDetection = useRef<number | null>(null); // 타입을 number로 선언
-  const [featuresDetected, setFeaturesDetected] = useState({
-    hasEyes: false,
-    hasNose: false,
-    hasMouth: false,
-  });
+  const [modelsLoaded, setModelsLoaded] = useState(false);
+  // const [featuresDetected, setFeaturesDetected] = useState({
+  //   hasLeftEyes: false,
+  //   hasRightEyes: false,
+  //   hasNose: false,
+  //   hasMouth: false,
+  // });
 
   useEffect(() => {
-    if (
-      faceapi.nets.ssdMobilenetv1.isLoaded &&
-      faceapi.nets.faceLandmark68Net.isLoaded
-    ) {
-      startVideo();
-      return;
-    }
-
     const loadModels = async () => {
       const MODEL_URL = "src/model";
       try {
         await Promise.all([
           faceapi.nets.ssdMobilenetv1.loadFromUri(MODEL_URL),
           faceapi.nets.faceLandmark68Net.loadFromUri(MODEL_URL),
+          faceapi.nets.faceRecognitionNet.loadFromUri(MODEL_URL),
         ]);
-        startVideo();
+        setModelsLoaded(true);
+        // startVideo();
       } catch (error) {
         console.error("Model loading failed", error);
         toast.error("Failed to load face detection models.");
       }
     };
-
     loadModels();
   }, []);
+
+  useEffect(() => {
+    if (modelsLoaded) {
+      startVideo();
+    }
+  }, [modelsLoaded]);
 
   // cleanup function for webcam usage
   useEffect(() => {
@@ -47,7 +47,7 @@ const FaceRecognizerCam = () => {
         track.stop();
       });
     };
-  }, [camMediaStream]);
+  }, []);
 
   const startVideo = () => {
     navigator.mediaDevices
@@ -59,20 +59,93 @@ const FaceRecognizerCam = () => {
       })
       .then((stream) => {
         camMediaStream.current = stream;
-        const video = videoRef.current!;
-        video.srcObject = stream;
-        video.play();
+        if (videoRef.current) {
+          videoRef.current.srcObject = stream;
+          videoRef.current.play();
+        }
+        // const video = videoRef.current!;
+        // video.srcObject = stream;
+        // video.play();
       })
       .catch((err) => {
         console.error(err);
-        toast.error(
-          "Problem accessing the webcam. Ensure permission has been granted."
-        );
+        toast.error("Problem accessing the webcam. Ensure permission has been granted.");
       });
   };
 
+  // useEffect(() => {
+  //   if (intervalDetection.current) clearInterval(intervalDetection.current);
+
+  //   detectFaceFeatures();
+
+  //   return () => {
+  //     if (intervalDetection.current) clearInterval(intervalDetection.current);
+  //   };
+  // }, [videoRef.current]);
+
+  // sets intervalTimer that keeps detecting face features
+  // const detectFaceFeatures = () => {
+  //   if (!videoRef.current) return;
+
+  //   intervalDetection.current = setInterval(async () => {
+  //     const detections = await faceapi
+  //       .detectAllFaces(videoRef.current)
+  //       .withFaceLandmarks();
+
+  //     if (detections.length > 0) {
+  //       // console.log(detections);
+  //       const { landmarks } = detections[0];
+  //       const hasEyes =
+  //         landmarks.getLeftEye().length > 0 &&
+  //         landmarks.getRightEye().length > 0;
+  //       const hasNose = landmarks.getNose().length > 0;
+  //       const hasMouth = landmarks.getMouth().length > 0;
+  //       console.log("eyes : " , hasEyes, " nose : ", hasNose, " mouth : ", hasMouth);
+
+  //       setFeaturesDetected({ hasEyes, hasNose, hasMouth });
+  //     }
+  //   }, 1000) as unknown as number; // NodeJS.Timer를 number로 강제 변환
+  // };
+
   useEffect(() => {
-    if (intervalDetection.current) clearInterval(intervalDetection.current);
+    const detectFaceFeatures = async () => {
+      const optionsSSDMobileNet = new faceapi.SsdMobilenetv1Options({
+        minConfidence: 0.2,
+      });
+
+      if (!videoRef.current) return;
+
+      intervalDetection.current = window.setInterval(async () => {
+        const detections = await faceapi
+          .detectSingleFace(videoRef.current, optionsSSDMobileNet)
+          .withFaceLandmarks();
+
+        if (detections) {
+          setFeaturesDetected((prevState) => ({
+            hasLeftEyes: detections.landmarks.getLeftEye().length > 0,
+            hasRightEyes: detections.landmarks.getRightEye().length > 0,
+            hasNose: detections.landmarks.getNose().length > 0,
+            hasMouth: detections.landmarks.getMouth().length > 0,
+          }));
+
+          console.log(detections?.landmarks.getLeftEyeBrow());
+          console.log(detections?.landmarks.getLeftEye());
+          console.log(detections?.detection.score);
+        } else {
+          
+          setFeaturesDetected((prevState) => ({
+            hasLeftEyes: false,
+            hasRightEyes: false,
+            hasNose: false,
+            hasMouth: false,
+          }));
+          console.log("no face");
+        } 
+
+        
+        // console.log(featuresDetected);
+      }, 1000);
+    };
 
     detectFaceFeatures();
 
@@ -81,34 +154,13 @@ const FaceRecognizerCam = () => {
     };
   }, [videoRef.current]);
 
-  // sets intervalTimer that keeps detecting face features
-  const detectFaceFeatures = () => {
-    if (!videoRef.current) return;
-
-    intervalDetection.current = setInterval(async () => {
-      const detections = await faceapi
-        .detectAllFaces(videoRef.current)
-        .withFaceLandmarks();
-      if (detections.length > 0) {
-        console.log(detections);
-        const { landmarks } = detections[0];
-        const hasEyes =
-          landmarks.getLeftEye().length > 0 &&
-          landmarks.getRightEye().length > 0;
-        const hasNose = landmarks.getNose().length > 0;
-        const hasMouth = landmarks.getMouth().length > 0;
-
-        setFeaturesDetected({ hasEyes, hasNose, hasMouth });
-      }
-    }, 1000) as unknown as number; // NodeJS.Timer를 number로 강제 변환
-  };
-
   return (
     <div className="relative">
-      <video ref={videoRef} />
-      <p>Has eyes: {featuresDetected.hasEyes.toString()}</p>
+      <video ref={videoRef} style={{ transform: "scaleX(-1)" }} />
+      {/* <p>Has Left eyes: {featuresDetected.hasLeftEyes.toString()}</p>
+      <p>Has Right eyes: {featuresDetected.hasRightEyes.toString()}</p>
       <p>Has nose: {featuresDetected.hasNose.toString()}</p>
-      <p>Has mouth: {featuresDetected.hasMouth.toString()}</p>
+      <p>Has mouth: {featuresDetected.hasMouth.toString()}</p> */}
     </div>
   );
 };
