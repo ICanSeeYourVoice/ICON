5c01209ac52728135a62a4ab72fd8aab846058f9
nayeon <kknyapple@gmail.com>
2024-05-12 20:10:12 +0900

    style: 설정 버튼 css

--- front/src/components/main/detection/SettingButton.tsx
+++ front/src/components/main/detection/SettingButton.tsx
@@ -18,30 +18,30 @@ const SettingButton = () => {
         } ${isSettingOpen ? "translate-y-0" : "translate-y-[-0.5rem]"}`}
       >
         {isSettingOpen && (
-          <div className="flex flex-col justify-center items-end gap-3">
-            <div className="flex flex-row gap-2 text-sm items-center justify-center">
+          <div className="flex flex-col justify-center items-end gap-2">
+            <div className="flex flex-row gap-2 text-sm items-center justify-center pl-[0.7rem] pr-[0.7rem] pt-[0.2rem] pb-[0.2rem] bg-white rounded-[1.5rem]">
               <div>
-                <span className="text-white ">카메라</span>
+                <span className="text-black text-base">카메라</span>
               </div>
-              <div className="flex items-center justify-center w-[2rem] h-[2rem] bg-white bg-opacity-15 rounded-full">
+              <div className="flex items-center justify-center w-[2rem] h-[2rem] bg-black bg-opacity-15 rounded-full">
                 <span className="text-xs font-bold">OFF</span>
               </div>
             </div>
 
-            <div className="flex flex-row gap-2 text-sm items-center justify-center">
+            <div className="flex flex-row gap-2 text-sm items-center justify-center pl-[0.7rem] pr-[0.7rem] pt-[0.2rem] pb-[0.2rem] bg-white rounded-[1.5rem]">
               <div>
-                <span className="text-white ">울음</span>
+                <span className="text-black text-base">울음</span>
               </div>
-              <div className="flex items-center justify-center w-[2rem] h-[2rem] bg-white bg-opacity-15 rounded-full">
+              <div className="flex items-center justify-center w-[2rem] h-[2rem] bg-black bg-opacity-15 rounded-full">
                 <span className="text-xs font-bold">ON</span>
               </div>
             </div>
 
-            <div className="flex flex-row gap-2 text-sm items-center justify-center">
+            <div className="flex flex-row gap-2 text-sm items-center justify-center pl-[0.7rem] pr-[0.7rem] pt-[0.2rem] pb-[0.2rem] bg-white rounded-[1.5rem]">
               <div>
-                <span className="text-white">행동</span>
+                <span className="text-black text-base">행동</span>
               </div>
-              <div className="flex items-center justify-center w-[2rem] h-[2rem] bg-white bg-opacity-15 rounded-full">
+              <div className="flex items-center justify-center w-[2rem] h-[2rem] bg-black bg-opacity-15 rounded-full">
                 <span className="text-xs font-bold">OFF</span>
               </div>
             </div>
