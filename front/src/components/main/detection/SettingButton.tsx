edfd918df19a330ccf6911dde8314830d7f6ef58
nayeon <kknyapple@gmail.com>
2024-05-12 18:36:00 +0900

    style: detection page 설정 버튼 애니메이션 추가

--- front/src/components/main/detection/SettingButton.tsx
+++ front/src/components/main/detection/SettingButton.tsx
@@ -12,36 +12,42 @@ const SettingButton = () => {
       >
         <img src={setting} alt="" />
       </div>
-      {isSettingOpen && (
-        <>
-          <div className="flex flex-row gap-2 text-sm items-center justify-center">
-            <div>
-              <span className="text-white ">카메라</span>
+      <div
+        className={`transition-all duration-400 ${
+          isSettingOpen ? "opacity-100" : "opacity-0"
+        } ${isSettingOpen ? "translate-y-0" : "translate-y-[-0.5rem]"}`}
+      >
+        {isSettingOpen && (
+          <div className="flex flex-col justify-center items-end gap-3">
+            <div className="flex flex-row gap-2 text-sm items-center justify-center">
+              <div>
+                <span className="text-white ">카메라</span>
+              </div>
+              <div className="flex items-center justify-center w-[2rem] h-[2rem] bg-white bg-opacity-15 rounded-full">
+                <span className="text-xs font-bold">OFF</span>
+              </div>
             </div>
-            <div className="flex items-center justify-center w-[2rem] h-[2rem] bg-white bg-opacity-15 rounded-full">
-              <span className="text-xs font-bold">OFF</span>
-            </div>
-          </div>
 
-          <div className="flex flex-row gap-2 text-sm items-center justify-center">
-            <div>
-              <span className="text-white ">울음</span>
+            <div className="flex flex-row gap-2 text-sm items-center justify-center">
+              <div>
+                <span className="text-white ">울음</span>
+              </div>
+              <div className="flex items-center justify-center w-[2rem] h-[2rem] bg-white bg-opacity-15 rounded-full">
+                <span className="text-xs font-bold">ON</span>
+              </div>
             </div>
-            <div className="flex items-center justify-center w-[2rem] h-[2rem] bg-white bg-opacity-15 rounded-full">
-              <span className="text-xs font-bold">ON</span>
-            </div>
-          </div>
 
-          <div className="flex flex-row gap-2 text-sm items-center justify-center">
-            <div>
-              <span className="text-white">행동</span>
-            </div>
-            <div className="flex items-center justify-center w-[2rem] h-[2rem] bg-white bg-opacity-15 rounded-full">
-              <span className="text-xs font-bold">OFF</span>
+            <div className="flex flex-row gap-2 text-sm items-center justify-center">
+              <div>
+                <span className="text-white">행동</span>
+              </div>
+              <div className="flex items-center justify-center w-[2rem] h-[2rem] bg-white bg-opacity-15 rounded-full">
+                <span className="text-xs font-bold">OFF</span>
+              </div>
             </div>
           </div>
-        </>
-      )}
+        )}
+      </div>
     </div>
   );
 };
