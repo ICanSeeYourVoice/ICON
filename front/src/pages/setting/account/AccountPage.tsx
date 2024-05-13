423483e52984cfc0050713780c67ccda00557aa7
nayeon <kknyapple@gmail.com>
2024-05-13 23:03:53 +0900

    fix: memberInfo 옵셔널 체이닝 추가

--- front/src/pages/setting/account/AccountPage.tsx
+++ front/src/pages/setting/account/AccountPage.tsx
@@ -38,12 +38,12 @@ const AccountPage = () => {
       <main className="flex flex-col items-center justify-center w-[80%] mt-[6rem] gap-5">
         <DisabledInput
           type="text"
-          value={(memberInfo as MemberInfoProps).name}
+          value={(memberInfo as MemberInfoProps)?.name}
           label="이름"
         />
         <DisabledInput
           type="text"
-          value={(memberInfo as MemberInfoProps).uid}
+          value={(memberInfo as MemberInfoProps)?.uid}
           label="아이디"
         />
         <DisabledInput type="password" value="******" label="비밀번호" />
