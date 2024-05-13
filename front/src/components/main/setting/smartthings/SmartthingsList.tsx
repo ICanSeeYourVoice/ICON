7ad11d3f4641ba69e1f051bec822e5c17ce55a1f
탁하윤 <thagyun@naver.com>
2024-05-13 14:26:10 +0900

    style: edit smartthings design

--- front/src/components/main/setting/smartthings/SmartthingsList.tsx
+++ front/src/components/main/setting/smartthings/SmartthingsList.tsx
@@ -1,44 +1,24 @@
 import { useNavigate } from "react-router-dom";
 import { SMARTTHINGS } from "../../../../constants/smartthings";
 
-interface RoutineItem {
-  scene_id: string;
-  name: string;
-  trigger?: string;
-}
-
-interface ListDataProps {
-  routineData: RoutineItem[];
-}
-
-const SmartthingsList: React.FC<ListDataProps> = ({ routineData }) => {
+const SmartthingsList: React.FC = () => {
   const navigate = useNavigate();
   const handleRoutine = (type: string) => () => {
     return navigate("/setting/things/routine", { state: { type } });
   };
 
-  const findRoutineNameByTrigger = (trigger: string) => {
-    const foundRoutine = routineData.find(
-      (routine) => routine.trigger === trigger
-    );
-    return foundRoutine ? foundRoutine.name : "설정하기";
-  };
-
   return (
-    <div className="flex flex-wrap w-full justify-center items-center gap-[0.5rem] h-[70vh] overflow-y-auto">
+    <div className="grid grid-cols-2 gap-2 w-full justify-center justify-items-center items-center content-start h-[70vh] overflow-y-auto">
       {Object.entries(SMARTTHINGS).map(([key, value], idx) => (
         <div
           key={idx}
-          className="flex flex-row justify-between w-full p-[1rem] rounded-[0.625rem] h-[5rem]"
+          className="flex w-full justify-center p-[1rem] rounded-[0.625rem] h-[8rem]"
           style={{ backgroundColor: value.COLOR }}
           onClick={handleRoutine(key)}
         >
           <div className="flex flex-col justify-center items-center">
-            <img src={value.ICON} alt={value.LABEL} className="w-8 h-8" />
+            <img src={value.ICON} alt={value.LABEL} className="w-10 h-10" />
             <div className="mt-[0.2rem] text-black">{value.LABEL}</div>
-          </div>
-          <div className="flex flex-col justify-center items-center">
-            <div className="text-black">{findRoutineNameByTrigger(key)}</div>
           </div>
         </div>
       ))}
