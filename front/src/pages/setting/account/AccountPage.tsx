8f83d08d786400f5b1f4b76dcd9e0cce2e36c2da
nayeon <kknyapple@gmail.com>
2024-05-13 23:01:57 +0900

    feat: 계정 페이지

--- front/src/pages/setting/account/AccountPage.tsx
+++ front/src/pages/setting/account/AccountPage.tsx
@@ -1,17 +1,52 @@
 import Nav from "../../../components/common/Navigator/Nav";
 import TopBar from "../../../components/common/Navigator/TopBar";
-import { useTokenStore } from "../../../stores/notification";
+import useMemberInfo from "../../../hooks/useMemberInfo";
+
+const DisabledInput = ({
+  type,
+  label,
+  value,
+}: {
+  type: string;
+  label: string;
+  value: string;
+}) => {
+  return (
+    <div>
+      <div className="text-black text-sm mb-2">{label}</div>
+      <div
+        className="w-[16rem] h-[3rem] relative flex-col justify-start 
+        items-start inline-flex  border rounded-[1rem] border-gray-300"
+      >
+        <input
+          type={type}
+          value={value}
+          className="w-full h-full rounded-[1rem] bg-white text-gray-0 text-sm p-4"
+          disabled
+        />
+      </div>
+    </div>
+  );
+};
 
 const AccountPage = () => {
-  const { token } = useTokenStore();
+  const { memberInfo } = useMemberInfo();
 
   return (
     <div className="flex flex-col items-center h-screen w-screen">
       <TopBar text="계정" path="setting" />
-      <main className="flex flex-col items-center justify-center w-[80%] mt-[6rem]">
-        <p className="flex flex-col items-center justify-center w-full break-all">
-          {token}
-        </p>
+      <main className="flex flex-col items-center justify-center w-[80%] mt-[6rem] gap-5">
+        <DisabledInput
+          type="text"
+          value={(memberInfo as MemberInfoProps).name}
+          label="이름"
+        />
+        <DisabledInput
+          type="text"
+          value={(memberInfo as MemberInfoProps).uid}
+          label="아이디"
+        />
+        <DisabledInput type="password" value="******" label="비밀번호" />
       </main>
       <Nav />
     </div>
