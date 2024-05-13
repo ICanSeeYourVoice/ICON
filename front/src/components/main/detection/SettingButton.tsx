e1731e4a29411c4ee727c26bb844da4464279482
nayeon <kknyapple@gmail.com>
2024-05-13 10:35:11 +0900

    feat: 감지 페이지 비디오 버튼 퍼블리싱

--- front/src/components/main/detection/SettingButton.tsx
+++ front/src/components/main/detection/SettingButton.tsx
@@ -1,61 +1,92 @@
 import { useState } from "react";
 import setting from "../../../assets/svgs/detection/setting.svg";
+import video from "../../../assets/svgs/detection/video.svg";
+import novideo from "../../../assets/svgs/detection/novideo.svg";
 import { useToggle } from "../../../stores/detection";
 
 const SettingButton = () => {
   const [isSettingOpen, setIsSettingOpen] = useState(true);
-  const { isCryDetect, isFaceDetect, setIsCryDetect, setIsFaceDetect } =
-    useToggle();
+  const {
+    isCryDetect,
+    isFaceDetect,
+    isCamera,
+    setIsCryDetect,
+    setIsFaceDetect,
+    setIsCamera,
+  } = useToggle();
 
   return (
-    <div className="flex flex-col justify-center items-end absolute top-0 right-0 mt-[1.8rem] mr-[2rem] gap-3">
-      <div
-        className="flex items-center justify-center w-[2.5rem] h-[2.5rem] bg-white bg-opacity-15 rounded-full cursor-pointer"
-        onClick={() => setIsSettingOpen(!isSettingOpen)}
+    <div>
+      <button
+        className={`flex flex-col justify-center items-end absolute top-0 right-0 mt-[1.8rem] mr-[5.2rem] gap-3 ${
+          isSettingOpen ? "opacity-40 pointer-events-none" : ""
+        }`}
       >
-        <img src={setting} alt="" />
-      </div>
-      <div
-        className={`transition-all duration-400 ${
-          isSettingOpen ? "opacity-100" : "opacity-0"
-        } ${isSettingOpen ? "translate-y-0" : "translate-y-[-0.5rem]"}`}
-      >
-        {isSettingOpen && (
-          <div className="flex flex-col justify-center items-end gap-2">
-            <div className="flex flex-row gap-2 text-sm items-center justify-center pl-[0.7rem] pr-[0.7rem] pt-[0.2rem] pb-[0.2rem] bg-white rounded-[1.5rem]">
-              <div>
-                <span className="text-black text-base">울음</span>
-              </div>
-              <button
-                onClick={() => {
-                  setIsCryDetect(!isCryDetect);
-                }}
-                className="flex items-center justify-center w-[2rem] h-[2rem] bg-black bg-opacity-15 rounded-full"
-              >
-                <span className="text-xs font-bold">
-                  {isCryDetect ? "ON" : "OFF"}
-                </span>
-              </button>
-            </div>
+        <div
+          className="flex items-center justify-center w-[2.5rem] h-[2.5rem] bg-white bg-opacity-15 rounded-full cursor-pointer"
+          onClick={() => {
+            setIsCamera(!isCamera);
+          }}
+        >
+          <img src={!isCamera ? novideo : video} alt="" />
+        </div>
+      </button>
 
-            <div className="flex flex-row gap-2 text-sm items-center justify-center pl-[0.7rem] pr-[0.7rem] pt-[0.2rem] pb-[0.2rem] bg-white rounded-[1.5rem]">
-              <div>
-                <span className="text-black text-base">행동</span>
+      <button
+        className={`flex flex-col justify-center items-end absolute top-0 right-0 mt-[1.8rem] mr-[2rem] gap-3 ${
+          isCamera ? "opacity-40 pointer-events-none" : ""
+        }`}
+      >
+        <div
+          className="flex items-center justify-center w-[2.5rem] h-[2.5rem] bg-white bg-opacity-15 rounded-full cursor-pointer"
+          onClick={() => {
+            setIsSettingOpen(!isSettingOpen);
+          }}
+        >
+          <img src={setting} alt="" />
+        </div>
+        <div
+          className={`transition-all duration-400 ${
+            isSettingOpen ? "opacity-100" : "opacity-0"
+          } ${isSettingOpen ? "translate-y-0" : "translate-y-[-0.5rem]"}`}
+        >
+          {isSettingOpen && (
+            <div className="flex flex-col justify-center items-end gap-2">
+              <div className="flex flex-row gap-1 text-sm items-center justify-center pl-[0.7rem] pr-[0.7rem] pt-[0.2rem] pb-[0.2rem] bg-white rounded-[1.5rem]">
+                <div>
+                  <span className="text-black text-sm">울음</span>
+                </div>
+                <button
+                  onClick={() => {
+                    setIsCryDetect(!isCryDetect);
+                  }}
+                  className="flex items-center justify-center w-[1.8rem] h-[1.8rem] bg-black bg-opacity-15 rounded-full"
+                >
+                  <span className="text-xs font-bold">
+                    {isCryDetect ? "ON" : "OFF"}
+                  </span>
+                </button>
               </div>
-              <button
-                onClick={() => {
-                  setIsFaceDetect(!isFaceDetect);
-                }}
-                className="flex items-center justify-center w-[2rem] h-[2rem] bg-black bg-opacity-15 rounded-full"
-              >
-                <span className="text-xs font-bold">
-                  {isFaceDetect ? "ON" : "OFF"}
-                </span>
-              </button>
+
+              <div className="flex flex-row gap-1 text-sm items-center justify-center pl-[0.7rem] pr-[0.7rem] pt-[0.2rem] pb-[0.2rem] bg-white rounded-[1.5rem]">
+                <div>
+                  <span className="text-black text-sm">행동</span>
+                </div>
+                <button
+                  onClick={() => {
+                    setIsFaceDetect(!isFaceDetect);
+                  }}
+                  className="flex items-center justify-center w-[1.8rem] h-[1.8rem] bg-black bg-opacity-15 rounded-full"
+                >
+                  <span className="text-xs font-bold">
+                    {isFaceDetect ? "ON" : "OFF"}
+                  </span>
+                </button>
+              </div>
             </div>
-          </div>
-        )}
-      </div>
+          )}
+        </div>
+      </button>
     </div>
   );
 };
