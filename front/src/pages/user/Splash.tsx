46c608e3dd44a9581fc01df5142471753d64f464
nayeon <kknyapple@gmail.com>
2024-05-20 10:25:39 +0900

    feat: 문구 수정

--- front/src/pages/user/Splash.tsx
+++ front/src/pages/user/Splash.tsx
@@ -21,7 +21,7 @@ const SplashPage = () => {
       <div className="flex flex-col items-center w-screen  pb-[4rem] gap-[1rem] leading-10">
         <img src={Logo} alt="Logo" className="mb-[1rem]" />
         <div className="h-[6rem] text-center text-slate-400  font-bold ">
-          AI와 스마트싱스 홈 카메라를 활용한 <br />
+          AI와 스마트싱스를 활용한 <br />
           청각 장애인 부모를 위한 아기 돌봄 보조 서비스
         </div>
       </div>
